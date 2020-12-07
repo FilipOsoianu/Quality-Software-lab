bc42327bb0ef475d91b49cc77c68b7bc4932067a
Filip Osoianu <filip.osoianu@sens.media>
2020-12-07 13:29:06 +0200

    add reports

--- src/test/java/TestRunner.java
+++ src/test/java/TestRunner.java
@@ -5,7 +5,8 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"."}
+        glue = {"."},
+        plugin = {"json:report/cucumber.json", "pretty","html:report/cucumber-reports.html"}
 )
 
 
